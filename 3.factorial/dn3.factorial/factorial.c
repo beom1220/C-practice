76776e802ddd8e1e33a3b538f47aace23f9b9937
beom1220 <117291257+beom1220@users.noreply.github.com>
2023-03-10 16:24:02 +0900

    50팩토리얼 uLL타입 시도
    가능한 한 가장 큰 표현범위를 가지는 타입인 unsigned long long으로 재시도.
    
    아직도 아득히 부족. 다른 해결방법 연구 필요.

--- 3.factorial/dn3.factorial/factorial.c
+++ 3.factorial/dn3.factorial/factorial.c
@@ -1,35 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int facCal(int a);
+int facCal(unsigned long long a);
 
 int main(void)
 {
-	int getNum = 1;
-	for (getNum; getNum != 0;)
+	unsigned long long getNum = 1uLL;
+	for (getNum; getNum != 0uLL;)
 	{
 		printf("팩토리얼 계산기입니다. 팩토리얼을 계산하고 싶은 50 이하의 숫자를 입력하세요. (종료는 0 입력) : ");
 		scanf("%d", &getNum);
-		if (getNum < 0)
-		{
-			getNum = 0;
-		}
 		facCal(getNum);
 	}
 }
 
 int facCal(a)
 {
-	int result = 1;
-	for (int i = 1; i <= a; i++)
+	unsigned long long result = 1uLL;
+	for (unsigned long long i = 1uLL; i <= a; i++)
 	{
 		result *= i;
 	}
-	if (a != 0)
-		printf("결과 : %d\n\n", result);
+	if (a != 0uLL)
+		printf("결과 : %50d\n\n", result);
 }
-
-// 일단 문제는 숫자가 커지면 터진다 (12까지만 정상적으로 나옴) -> 표현 가능한 범위를 벗어났다 -> 그걸 어케 고치는가.
-// 12! = 479,001,600 (4억 7900만 가량) / 13! = 6,227,020,800 (62억 2700만 가량) / int 범위는 2,147,483,647 (21억 4700만 가량)
-// 확실히 터진 이유는 int 범위를 벗어나서. 근데 왜 long이나 long long, unsigned long long 타입으로 바꿔도 안 되는가.
-// 아아아ㅏㅏㅏ 왜냐고 왜.
